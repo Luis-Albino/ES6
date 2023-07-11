c95d4355926ff7e47dcf746ba5c4616e0138bc90
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-07-11 13:11:45 -0600

    Update exer39.js

--- exer39.js
+++ exer39.js
@@ -1,9 +1,26 @@
 function validate (str) {
-    let valid = !str.match(/\w{4}/g);
-    return valid
+    let forbidden = ["1234567890","qwertyuiop","asdfghjklñ","zxcvbnm"];
+    for (let validationStr of forbidden) {
+        let i = 0;
+        let index = -1;
+        for (let j=0; j<str.length; j++) {
+            let previousIndex = index;
+            index = validationStr.indexOf(str[j].toLowerCase());
+            if (index > -1 && index === previousIndex + 1) {
+                i++
+            }
+            else {
+                i = 0
+            };
+            if (i === 4) {
+                return false
+            }
+        }
+    }
+    return true
 };
 
 console.log(validate("1234"))
 console.log(validate("qwert"))
 console.log(validate("asdf"))
-console.log(validate("123 456 789"))
+console.log(validate("Read a book every night"))
