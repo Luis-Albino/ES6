d2d446a60a34d3ea0f87b640de1c2314556ec568
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-07-11 13:12:21 -0600

    Update exer40.js

--- exer40.js
+++ exer40.js
@@ -1,15 +1,27 @@
-Array.prototype.searchSet = function () {
+Array.prototype.searchSet = function ()
+{   
     let length = this.length;
-    let array = [];
-    for (let i=0; i<length-2; i++) {
-        for (let j=i+1; j<length-1; j++) {
-            for (let k=j+1; k<length; k++) {
-                let sum = this[i]+this[j]+this[k];
-                if (sum === 0) array.push([this[i],this[j],this[k]]);
+    let array = []
+ 
+    for (var i=0; i<length-1; i++)
+    {
+        let el1 = this[i];
+        let hashSet = [];
+        for (var j=i+1; j<length; j++)
+        {
+            let el2 = this[j];
+            let hashElement = -(el1 + el2);
+            if (hashSet.includes(hashElement))
+            {
+                array.push([hashElement,el1,el2])
             }
+            else
+                hashSet.push(el2);
         }
     }
-    return array
-};
 
-console.log([3,2,1,0,-3,2,2,-2].searchSet())
+    return array
+}
+
+const arr = [0, -1, 2, -3, 1];
+console.log(arr.searchSet())
