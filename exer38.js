bf32401720925c1a87079f70a40e2d77be24ad06
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-07-11 13:11:08 -0600

    Update exer38.js

--- exer38.js
+++ exer38.js
@@ -2,57 +2,58 @@
 // Athlete constructor //
 /////////////////////////
 
-function Athlete (firstName,lastName,height,weigth) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.height = height; // in meters
-    this.weigth = weigth; // in lbs
-
-    // medals //
-    let medals = {
-        gold: 0,
-        silver: 0,
-        bronze: 0
-    };
-
-    this.winMedal = function (type) {
-        type = type.toLowerCase();
-        if (medals.hasOwnProperty(type)) medals[type] += 1;
-    };
-
-    this.showMedals = function (type) {
-        return medals
+class Athlete {
+    constructor (firstName,lastName,height,weigth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.height = height; // in meters
+        this.weigth = weigth; // in lbs
+    
+        // medals //
+        let medals = {
+            gold: 0,
+            silver: 0,
+            bronze: 0
+        };
+    
+        this.winMedal = function (type) {
+            type = type.toLowerCase();
+            if (medals.hasOwnProperty(type)) medals[type] += 1;
+        };
+    
+        this.showMedals = function (type) {
+            return medals
+        }
     }
-};
 
-Athlete.prototype.introduce = function () {
-    console.log("This is",this.firstName,this.lastName)
-};
+    introduce () {
+        console.log("This is",this.firstName,this.lastName)
+    }
 
-Athlete.prototype.BMI = function () {
-    // lbs to kg convertion //
-    let kg = 0.45359*this.weigth;
-    let height2 = (this.height)*(this.height);
-    return kg/height2
-};
+    BMI () {
+        // lbs to kg convertion //
+        let kg = 0.45359*this.weigth;
+        let height2 = (this.height)*(this.height);
+        return kg/height2
+    }
+}
 
 /////////////////////////
 // Swimmer constructor //
 /////////////////////////
 
-function Swimmer (firstName,lastName,height,weigth) {
-    Athlete.apply(this,arguments);
-    this.training = function () {
-        /* start swimmer trainig */
+class Swimmer extends Athlete {
+    constructor (firstName,lastName,height,weigth) {
+        super(firstName,lastName,height,weigth)
+        this.training = function () {
+            /* start swimmer trainig */
+        }
     }
-};
 
-Object.setPrototypeOf(Swimmer.prototype,Athlete.prototype);
-
-Swimmer.prototype.introduce = function () {
-    console.log(this.firstName,this.lastName,"is a professional swimmer")
-};
-
+    introduce () {
+        console.log(this.firstName,this.lastName,"is a professional swimmer")
+    }
+}
 
 /////////////////////////
 /// Swimmer instance ////
