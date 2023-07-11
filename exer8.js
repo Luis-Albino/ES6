c7355eda9ab2fb542772e869d23a61eb83b65d40
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-07-11 13:02:05 -0600

    Update exer8.js

--- exer8.js
+++ exer8.js
@@ -1,1 +1,20 @@
-console.log([2,0,4,5,0,2,0,0,3].sort((a, b) => b===0?-1:1))
+// console.log(arr.sort((a, b) => b===0?-1:1))
+
+Array.prototype.moveZeroes = function () {
+    for (let i=0; i < this.length-1; i++) {
+        let j = i+1;
+        while (!this[i] && j < this.length) {
+            if (this[j]) {
+                [this[i],this[j]] = [this[j],this[i]]
+            }
+            else {
+                j++
+            }
+        }
+    }
+    return this
+}
+
+let arr = [2,0,4,5,0,2,0,0,3];
+arr.moveZeroes()
+console.log(arr)
