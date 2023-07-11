6a2961e25962798b8313bdeca0e7b1530b39e622
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-07-11 13:01:38 -0600

    Update exer6.js

--- exer6.js
+++ exer6.js
@@ -1,11 +1,15 @@
-String.prototype.appendAfter = function (apendedStr,referenceStr) {
+String.prototype.appendMonthAfter = function (referenceMonth) {
+    let month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let monthIndex = month.indexOf(referenceMonth);
+    monthIndex = (monthIndex > 10)?0:monthIndex+1;
+    let nextMonth = month[monthIndex];
     let str = "";
-    let index = this.search(referenceStr);
+    let index = this.search(referenceMonth);
     if (index > -1) {
         let slice = this.slice(0,index);
-        let rest = this.slice(index + referenceStr.length);
-        rest = rest.appendAfter(apendedStr,referenceStr)
-        str = slice + referenceStr + apendedStr + rest
+        let rest = this.slice(index + referenceMonth.length);
+        rest = rest.appendMonthAfter(referenceMonth)
+        str = slice + referenceMonth + " " + nextMonth + rest
     }
     else {
         str = this.toString()
@@ -13,4 +17,4 @@ String.prototype.appendAfter = function (apendedStr,referenceStr) {
     return str
 };
 
-console.log("January February March January February March".appendAfter(" April","March"))
+console.log("January February March January February March".appendMonthAfter("March"))
