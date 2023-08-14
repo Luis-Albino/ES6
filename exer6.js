a8dc8490b0eddc90baa739dd8c2239447628ca0f
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-08-14 16:26:46 -0600

    Update exer6.js

--- exer6.js
+++ exer6.js
@@ -1,20 +1,23 @@
-String.prototype.appendMonthAfter = function (referenceMonth) {
-    let month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-    let monthIndex = month.indexOf(referenceMonth);
-    monthIndex = (monthIndex > 10)?0:monthIndex+1;
-    let nextMonth = month[monthIndex];
-    let str = "";
-    let index = this.search(referenceMonth);
-    if (index > -1) {
-        let slice = this.slice(0,index);
-        let rest = this.slice(index + referenceMonth.length);
-        rest = rest.appendMonthAfter(referenceMonth)
-        str = slice + referenceMonth + " " + nextMonth + rest
-    }
-    else {
-        str = this.toString()
-    }
-    return str
-};
+ function appendNextMonth (str) {
+    let monthNames = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let monthArray = str.match(/\w+/g);
+    let lastIndex = monthArray.lastIndexOf(monthArray[0])-1;
+    let month = monthArray[lastIndex];
+    let date = new Date(month + " 1,0");
+    let newMonth = monthNames[date.getMonth()+1];
 
-console.log("January February March January February March".appendMonthAfter("March"))
+    let newStr = "";
+    for (let i in monthArray) {
+        newStr += monthArray[i] + " ";
+        if (i == lastIndex) {
+            newStr += newMonth + " ";
+        }
+    };
+    newStr += newMonth;
+
+    return newStr
+}
+
+let str = "January February March January February March";
+
+console.log(appendNextMonth(str))
