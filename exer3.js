9e9b09ea2cb9051755b9fc21c9e234487b4ed3c2
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-08-14 16:26:04 -0600

    Update exer3.js

--- exer3.js
+++ exer3.js
@@ -5,7 +5,8 @@ let arr2 = [3, 4, 3, 5, 3, 6, 8];
 function intersection (arr1,arr2) {
     let interArr = [];
     for (let el of arr1) {
-        if (arr2.includes(el) && !interArr.includes(el)) {
+        if (arr2.includes(el)) {
+            arr2.splice(arr2.indexOf(el),1)
             interArr.push(el)
         }
     }
@@ -13,7 +14,3 @@ function intersection (arr1,arr2) {
 }
 
 console.log(intersection(arr1,arr2));
-
-
-// version 2 //
-// console.log(arr1.filter(el => arr2.includes(el)));
