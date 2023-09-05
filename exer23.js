12ba7bdc8bd154f20fe84d0c13c4aef857f89ca1
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-09-04 19:10:50 -0600

    Update exer23.js

--- exer23.js
+++ exer23.js
@@ -1,30 +1,51 @@
-function* randomLCG (min = 1,max) {   
-    if (max === undefined) {
-        [max,min] = [min,0]
-    };
+function primeNumbers (max) {
+    let prime = [1];
+    for (let i=2; i<= max; i++) {
+        let isPrime = true;
+        for (let j=1; j<prime.length; j++) {
+            if (!(i%prime[j])) {
+                isPrime = false;
+                break; 
+            };
+        }
+        if (isPrime) prime.push(i)
+    }
+    return prime
+}
 
-    if (min > max) return null;
+function* randomLCG(min,max = 0) {
+    if (min > max) [max,min] = [min,max];
+    let modulus = max-min+1;
+    let seed = Date.now() % modulus; 
+    let a = 1;
+    let c;
 
-    let modulus = max - min;
-    let number;
-    let nextNumber = Date.now() % modulus; // seed
-    const A = 4;  // multiplier  A < modulus
-    let C = 0;    // incrementer c < modulus
-    while (C === 0) {
-        C = Math.floor(modulus*Math.random())
-
+    let isRational = true;
+    let prime = primeNumbers(modulus-1);
+    while (isRational) {
+        let length = prime.length;
+        let index = Date.now() % length;
+        if (index ===0 || modulus % prime[index]) {
+            isRational = false;
+            c = prime[index];
+        }
     }
 
-    for (let i=0; i<modulus; i++) {
+    nextNumber = seed;
+    for(let i = 0; i < modulus; i++) {
         number = nextNumber;
-        nextNumber = (A*number + C) % modulus;
+        nextNumber = (number*a + c) % modulus;
         yield min + number
     }
 }
 
-let min = 0;
-let max = 9;
+let min = 2;
+let max = 5;
+
+// Function Call
 let random = randomLCG(min,max);
-for(let i=0; i<max-min; i++) {
-    console.log(random.next().value)
+
+// Print the generated random numbers
+for(let i = 0; i < max-min+1; i++) {
+    console.log(random.next().value);
 }
