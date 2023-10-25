2abc45b282bf1e92ba34185e3a115679f2bd2694
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-10-25 17:08:54 -0600

    Update exer28.js

--- exer28.js
+++ exer28.js
@@ -2,23 +2,17 @@ var async = {
     getAll: async function (urlArray,callBack) {
         let promiseArray = urlArray.map(url => fetch(url));
 
-        let context = {};
-        let j = 0;
         await promiseArray.reduce((chain,promise) => chain
             .then(function () {
                 return promise
             })
             .then(response => response.json())
-            .then(response => {
-                context[j] = response;
-                j++;
-            })
+            .then(response => callBack(response))
+            .catch(err => callBack(err))
         ,Promise.resolve());
-
-        callBack(context)
-
     }
 }
+
 
 //////////////////////////////////////////////////////
 // Example: using the same localhost of exercise 18 //
@@ -26,12 +20,8 @@ var async = {
 
 let myUrlArray = ['http://localhost:3000/candidates/0','http://localhost:3000/candidates/1','http://localhost:3000/candidates/2','http://localhost:3000/candidates/3'];
 
-function myCallBack (responseObj) {
-    for (let key in responseObj) {
+function myCallBack (data) {
         let div = document.createElement("div");
-        div.innerHTML = responseObj[key]["firstname"] ?? "Not found";
+        div.innerHTML = data["firstname"] ?? "Not found";
         document.body.appendChild(div)
-    }
 }
-
-async.getAll(myUrlArray,myCallBack)
