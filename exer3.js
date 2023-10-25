6ad204cd9dee6c21cb16454feed3c8f734cba566
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-10-25 15:47:25 -0600

    Update exer3.js

--- exer3.js
+++ exer3.js
@@ -1,19 +1,23 @@
 let arr1 = [2, 3, 5, 3];
 let arr2 = [3, 4, 3, 5, 3, 6, 8];
 
-// version 1 //
 function intersection (arr1,arr2) {
-    let intersectionArr = [];
-    let intersectionIndices = {};
+    let intersection = [];
+    let obj = {};
+
     for (let el of arr1) {
-        intersectionIndices[el] = -1;
-        if (arr2.includes(el)) intersectionIndices[el] = (intersectionIndices[el] != undefined)?arr2.indexOf(el,intersectionIndices[el]+1):arr2.indexOf(el);
-        let index = intersectionIndices[el];
-        if (index >= 0) {
-            intersectionArr.push(el)
+        if (!obj[[el]]) obj[[el]] = 1
+        else obj[[el]]++;
+    }
+
+    for (let el of arr2) {
+        if (obj[[el]]) {
+            intersection.push(el);
+            obj[[el]] -= 1;
         }
     }
-    return intersectionArr
+
+    return intersection
 }
 
 console.log(intersection(arr1,arr2));
